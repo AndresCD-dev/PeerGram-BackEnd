4045ae36cd1d4e4a0c1bc03f61bb81a8942806c2
andres <andres.gordillo@mail.citytech.cuny.edu>
2021-11-29 17:34:46 -0500

    heroku change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://peer-gram.herokuapp.com'
+      origins 'https://peer-gram.herokuapp.com/'
   
       resource '*',
         headers: :any,
